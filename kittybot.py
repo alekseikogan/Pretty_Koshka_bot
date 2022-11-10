1dfa3f9a0872eb15b0e3dfee62a984ca3b202dbd
Алексей Коган <alekseikogan@yandex.ru>
2022-11-10 22:22:52 +0300

    дошел до домашки

--- kittybot.py
+++ kittybot.py
@@ -1,21 +1,30 @@
+import logging
 import os
+
 import requests
 
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import Updater, Filters, MessageHandler, CommandHandler
+
 from dotenv import load_dotenv
 
 load_dotenv()
+
 secret_token = os.getenv('TOKEN')
 updater = Updater(token=secret_token)
-URL = ''
+URL = 'https://api.thecatapi.com/v1/images/search'
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
 
 def get_new_image():
     try:
         response = requests.get(URL)
     except Exception as error:
-        print(error)
+        # print(error)
+        logging.error(f'Ошибка при запросе к основному API: {error}')
         new_url = 'https://api.thedogapi.com/v1/images/search'
         response = requests.get(new_url)
     response = response.json()
@@ -57,8 +66,15 @@ def wake_up(update, context):
     )
 
 
-updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, say_hello))
-updater.start_polling()
-updater.idle()
+def main():
+    updater = Updater(token=secret_token)
+
+    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+    updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__=='__main__':
+    main()
